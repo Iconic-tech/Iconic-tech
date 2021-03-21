021b791e28df506b39da39fc00afa8e8b941e521
Iconic-tech <80595226+Iconic-tech@users.noreply.github.com>
2021-03-21 09:20:03 -0700

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,17 @@
+const languages = [
+  {
+    enabled: true,
+    name: 'English',
+    tag: 'en'
+  },
+  {
+    enabled: true,
+    name: '中文',
+    tag: 'zh-CN'
+  },
+  {
+    enabled: true,
+    name: 'Русский',
+    tag: 'ru',
+  }
+]
